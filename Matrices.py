a9b3cdd43609cae292809f96e676a0d0c30a2a69
jtorresli <manu.jose.torres@gmail.com>
2020-08-11 20:15:47 -0500

    comentando funciones

--- Matrices.py
+++ Matrices.py
@@ -1,32 +1,32 @@
-# Function of feature and class matrix generation
+# Función de generación de matrices de clases y características
 def generateMatrice(data, K_mer, k):
 	# Variables
 	X = []
 	y = []
 
-	# Generate K-mer dictionnary
+	# Generar diccionario K-mer
 	X_dict = {}
 	for i, e in enumerate(K_mer):  X_dict[e] = 0;
-	# Generates X (matrix attributes)
+	# Generar X (atributos de matriz)
 	for d in data:
 		x = []
 		x_dict =  X_dict.copy()
 
-		# Count K-mer occurences (with overlaping)
+		# Contar ocurrencias de K-mer (con superposición)
 		for i in range(0, len(d[1]) - k + 1, 1):
 			try: x_dict[d[1][i:i + k]] = x_dict[d[1][i:i + k]] + 1; 
 			except: pass
 		
-		# Get only occurences from dictionnary
+		# Obtener todas las ocurrencias del diccionario
 		for value in x_dict:
 			x.append(x_dict.get(value))
 		X.append(x)
 
-	# Generates y (Matrix class) if csv file exist
+	# Genera y (clase Matrix) si existe un archivo csv
 	if len(data[0]) == 3: 
 		for i in data: y.append(i[2])
 	
-	# Return matrices X and y  (matrix attributes and matrix class)
+	# Retornar matrices X e y (atributos de matriz y clase de matriz)
 	return X, y
 
 
