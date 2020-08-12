50532a548f417759c0531550203bc4c70f560e8c
huisaluisqj <65868645+huisaluisqj@users.noreply.github.com>
2020-08-11 21:57:26 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,63 +4,64 @@ import sys
 import csv
 from Bio import SeqIO
 
-# Function checking the existence and accessibility of training files 
+# Función de comprobación de la existencia y accesibilidad de archivos de formación. 
 def checkTrainFile(training_fasta, training_csv):
-	# Check fasta file
+	# Ver archivo fasta
 	if os.path.isfile(training_fasta) and os.access(training_fasta, os.R_OK): 
 		print(training_fasta, "file exists and is readable")
-		# Check csv file
+		# Comprobar archivo csv
 		if os.path.isfile(training_csv) and os.access(training_csv, os.R_OK): 
 			print(training_csv, "file exists and is readable")
-			# Return True if all is correct
+			# Devuelve verdadero si todo es correcto
 			return True
-		# Exit and display a message in case of error 
+		# Salir y mostrar un mensaje en caso de error
 		else: sys.exit("Training csv file is missing or not readable")
 	else: sys.exit("Training fasta file is missing or not readable")
 
-# Function checking the existence and accessibility of testing files 
+# Función que comprueba la existencia y accesibilidad de los archivos de prueba.
 def checkTestFile(testing_fasta, testing_csv):
-	# Check fasta file
+	# Ver archivo fasta
 	if os.path.isfile(testing_fasta) and os.access(testing_fasta, os.R_OK): 
 		print(testing_fasta, "file exists and is readable")
-		# Check csv file
+		# Comprobar archivo csv
 		if os.path.isfile(testing_csv) and os.access(testing_csv, os.R_OK): 
-			# Return True if all is correct (prediction with evaluation)
+			# Devuelve True si todo es correcto (predicción con evaluación)
 			print(testing_csv, "file exists and is readable")
 			return True
 		else: 
-			# Return True if only fasta file is correct (prediction without evaluation)
+			# Devuelve True si solo el archivo fasta es correcto (predicción sin evaluación)
 			print("Testing csv file is missing or not readable")
 			return True
 	else: sys.exit("Testing fasta file is missing or not readable")
 
-# Function generating the data table 
+# Función que genera la tabla de datos
 def generateTrainData(fasta_file, csv_file):
 	# Variable data 
 	data = []
 
-	# Open the class file
+	# Abrir el archivo de la clase
 	with open(csv_file) as f: reader = dict(csv.reader(f))
 
-	# Open the sequences file
+	#Abrir el archivo de secuencias
 	for record in SeqIO.parse(fasta_file, "fasta"):
-		# Generate table [Id, Sequences, Class]
+		# Generar tabla [Id, Sequences, Class]
 		if record.id in reader: data.append([record.id, record.seq.upper(), reader[record.id]])
 
 	# Return data
 	return data
 
-# Function generating the data table 
+# Función que genera la tabla de datos
 def generateTestData(fasta_file, csv_file):
 	# Variable data 
 	data = []
 
-	# Call classical function
+	# Llamar a la función clásica
 	if csv_file: data = generateTrainData(fasta_file, csv_file)
 	else: 
-		# Open the sequences file and generate table [Id, Sequences]
+		# Abra el archivo de secuencias y genere la tabla [Id, Sequences]
 		for record in SeqIO.parse(fasta_file, "fasta"): data.append([record.id, record.seq.upper()])
 			
 	# Return data
 	return data
+
 
