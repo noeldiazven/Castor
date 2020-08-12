7a41f2ff8196f90e59d8b1e3da56e7b2a2cdac5d
jtorresli <manu.jose.torres@gmail.com>
2020-08-11 20:34:33 -0500

    comentando main

--- Main.py
+++ Main.py
@@ -1,69 +1,59 @@
-###############
-### IMPORTS ###
-############### 
+
+# IMPORTS
 import Data
 import Extraction
 import Evaluation
 
-####################
-### INFORMATIONS ###
-####################
+# INFORMACIÓN
 print("*******************")
 print("*** CASTOR-KRFE ***")
 print("*******************\n")
 
 print("Alignment-free method to extract discriminant genomic subsequences within pathogen sequences.\n")
 
-#################
-### VARIABLES ###
-#################
+# VARIABLES
 
-# Threshold (percentage of performance loss in terms of F-measure to reduce the number of attributes)
+# Umbral (porcentaje de pérdida de rendimiento en términos de medida F para reducir el número de atributos)
 T = 0.999
-# Minimum length of k-mer(s)
+# Longitud mínima de k-mer(s)
 k_min = 1
-# Maximum length of k-mer(s)
+# Longitud máxima de k-mer(s)
 k_max = 5
-# Minimum number of features to identify
+# Número mínimo de características para identificar
 features_min = 1
-# Maximum number of features to identify
+# Número maximo de características para identificar 
 features_max = 100
-# Training fasta file path
+# Ruta de archivo fasta de entrenamiento
 training_fasta = "Input/HIVGRPCG/data.fasta"
-# Training fasta file path
+# Ruta de archivo fasta de entrenamiento
 training_csv = "Input/HIVGRPCG/target.csv"
-# Testing fasta file path
+# Ruta de archivo fasta de pruebas
 testing_fasta = "Input/HIVGRPCG/data.fasta"
-# Testing fasta file path
+# Ruta de archivo fasta de pruebas
 testing_csv = "Input/HIVGRPCG/target.csv"
 
-###########################
-### LOAD TRAINING DATA  ###
-###########################
+
+# CARGAR DATOS DE ENTRENAMIENTO
 print("\nLoading of the training dataset...")
 if Data.checkTrainFile(training_fasta, training_csv) == True: training_data = Data.generateTrainData(training_fasta, training_csv)
 
-############################
-### FEATURES EXTRACTION  ###
-############################
+
+# EXTRACCIÓN DE CARACTERÍSTICAS
 print("\nStart feature extraction...")
 extracted_k_mers, identified_k_length = Extraction.extractKmers(T, training_data, k_min, k_max, features_min, features_max)
 
-########################
-### MODEL EVALUATION ###
-########################
+
+# EVALUACIÓN DEL MODELO
 print("\nEvaluation of the prediction model...")
 Evaluation.cross_validation(training_data, extracted_k_mers, identified_k_length, training_data)
 
-###########################
-### LOAD TESTING DATA  ###
-###########################
+
+# CARGAR DATOS DE PRUEBA
 print("\nLoading of the testing dataset...")
 if Data.checkTestFile(testing_fasta, testing_csv) == True: testing_data = Data.generateTestData(testing_fasta, testing_csv)
 
-###################
-### PREDICTION  ###
-###################
+
+# PREDICCION
 if len(testing_data[0]) == 2: 
 	print("\nPrediction without evaluation...")
 	Evaluation.prediction(training_data, testing_data, extracted_k_mers, identified_k_length)
@@ -71,8 +61,6 @@ else:
 	print("\nPrediction with evaluation...")
 	Evaluation.predictionEvaluation(training_data, testing_data, extracted_k_mers, identified_k_length)
 
-###########
-### END ###
-###########
+
 print("\nEnd of the program ")
 
