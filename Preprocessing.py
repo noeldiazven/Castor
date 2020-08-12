789882a3b69e6f8bc797e5bfed9f54b3ffc69506
noeldiazven <celso.diaz.ventura@gmail.com>
2020-08-11 22:04:27 -0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,7 +1,6 @@
-# Import
 from sklearn.preprocessing import MinMaxScaler
 
-# Function of MinMaxScaler (0, 1)
+# Funcion ode MinMaxScaler (0, 1)
 def minMaxScaling(X):
 	minMaxScaler = MinMaxScaler(feature_range = (0, 1), copy = False)
 	X = minMaxScaler.fit_transform(X)
